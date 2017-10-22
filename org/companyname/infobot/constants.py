dd2cef09eef709a0b44ddd1bd66444bbaef597f8
lytves <lytves@gmail.com>
2017-10-23 01:38:13 +0200

    new text constants

--- org/companyname/infobot/constants.py
+++ org/companyname/infobot/constants.py
@@ -1,1 +1,9 @@
 token = "put Token API of your Telegram Bot to file constants.py to variable token"
+
+start = "Start info of your Bot!"
+settings = "Settings of your Bot"
+help = "Help info about your Bot"
+
+answer_default = "I am a your InfoBot!"
+answer_hi = "Hi user!"
+answer_bye = "Bye-bye!"
