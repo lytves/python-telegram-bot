af31bc357bbf927e94e9b5d7cf4bb62e9e58eb48
lytves <lytves@gmail.com>
2017-10-22 02:18:36 +0200

    send a message from your bot to user

---
+++ org/companyname/infobot/constants.py
@@ -0,0 +1,1 @@
+# token = "put Token API of your Telegram Bot to file constants.py to variable token"
---
+++ org/companyname/infobot/main.py
@@ -0,0 +1,20 @@
+# my first Telegram Bot on Python3.5
+"""
+I learn Python3.5
+"""
+
+# to import a library pyTelegramBotAPI
+import telebot
+
+# to import a file with our constants
+from org.companyname.infobot import constants
+
+# put Token API of your Telegram Bot to file "constants.py" to variable "token"
+bot = telebot.TeleBot(constants.token)
+
+# send a message to chat with the number id
+# look your chat id with the method getUpdates from https://api.telegram.org/
+id = 1234567890
+
+bot.send_message(id, "test from MyBot")
+
